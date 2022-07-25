34ef3e83dc1f316e182457b0bf6b428f803be139
gabisegger <gabi.segger@gmail.com>
2022-07-25 08:03:26 -0300

    feat: task status

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const createItem = (tarefa) => {
+const createItem = (task, status='') => {
     const item = document.createElement('label');
     item.classList.add('todo_item');
     item.innerHTML = `
-        <input type="checkbox">
-        <div>${tarefa}</div>
+        <input type="checkbox" ${status}>
+        <div>${task}</div>
         <input type="button" value="X">
     `
     document.getElementById('todoList').appendChild(item);
