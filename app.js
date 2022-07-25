1354658176f80252bafa60bc3ad529063ca9e54a
gabisegger <gabi.segger@gmail.com>
2022-07-25 07:59:47 -0300

    feat: create item dynamically with custom task

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const createItem = (tarefa) => {
+    const item = document.createElement('label');
+    item.classList.add('todo_item');
+    item.innerHTML = `
+        <input type="checkbox">
+        <div>${tarefa}</div>
+        <input type="button" value="X">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
